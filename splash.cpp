50e1a2c760932da5f6bdaca8879251711484be93
disk91 <disk_91@homtail.com>
2021-04-26 12:57:04 +0200

    Add the GPL headers

--- splash.cpp
+++ splash.cpp
@@ -1,3 +1,20 @@
+/**
+ * This file is part of Wio LoRaWan Field Tester.
+ *
+ *   Wio LoRaWan Field Tester is free software created by Paul Pinault aka disk91. 
+ *   You can redistribute it and/or modify it under the terms of the 
+ *   GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   any later version.
+ *
+ *  Wio LoRaWan Field Tester is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Wio LoRaWan Field Tester.  If not, see <https://www.gnu.org/licenses/>.
+ */  
 #include <Arduino.h>
 #include "config.h"
 #include "helium_logo.h"
